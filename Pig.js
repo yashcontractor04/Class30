65787a970850d5bebb9088262f3209cb2628d7cf
yashcontractor04 <yashcontractor04@gmail.com>
2020-07-31 16:46:28 +0530

    DestroyedPig

--- Pig.js
+++ Pig.js
@@ -1,7 +1,21 @@
 class Pig extends BaseClass {
   constructor(x, y){
     super(x,y,50,50);
+    this.visibility=255;
     this.image = loadImage("sprites/enemy.png");
   }
-
+  display(){
+    if(this.body.speed<3){
+      super.display();
+    }
+    else{
+      var pos= this.body.position
+      World.remove(world,this.body);
+      push();
+      this.visibility=this.visibility-10;
+      tint(255,this.visibility);
+      image(this.image,pos.x,pos.y,50,50);
+      pop();
+    }
+  }
 };
